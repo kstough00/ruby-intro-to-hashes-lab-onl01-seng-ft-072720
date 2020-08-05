db3087d7339c929eaadbc9c091a3466f610b68d8
Kyle Stough <kstough00@gmail.com>
2020-08-05 15:19:35 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -16,9 +16,13 @@ def monopoly_with_second_tier
   monopoly = {railroads: {pieces: 4}}
 end
 
-def monopoly_with_third_tier
-	monopoly = {railroads: {pieces: 4}}
-
+def monopoly_with_second_tier
+	monopoly = {
+		railroads: {
+			pieces: 4, 
+			names: {}
+		}
+	}
 end
 
 def monopoly_with_second_tier
